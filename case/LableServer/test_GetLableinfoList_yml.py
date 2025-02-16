5f3c015f2577aee638ce2f62115b3cc433f912c4
v_vytxu <v_vytxu@tencent.com>
2025-02-16 21:58:34 +0800

    注释掉yml.py

--- case/LableServer/test_GetLableinfoList_yml.py
+++ case/LableServer/test_GetLableinfoList_yml.py
@@ -5,15 +5,15 @@ from utils.requestsUtil import RequestUtil
 
 
 class TestGetAllLabelInfoList:
-
+    pass
     # 标签列表接口
-    @pytest.mark.parametrize("caseinfo", read_testcase("GetLabelInfoList.yaml"))
-    def test_GetLabelInfoList_yml(self, caseinfo):
-        rsp = RequestUtil().standard_yaml(caseinfo)
-        AssertUtil().assertCommon(rsp)
-
-    # 标签详情接口
-    @pytest.mark.parametrize("caseinfo", read_testcase("GetLabelDetail.yaml"))
-    def test_GetLabelDetail(self, caseinfo):
-        rsp = RequestUtil().standard_yaml(caseinfo)
-        AssertUtil().assertCommon(rsp)
+    # @pytest.mark.parametrize("caseinfo", read_testcase("GetLabelInfoList.yaml"))
+    # def test_GetLabelInfoList_yml(self, caseinfo):
+    #     rsp = RequestUtil().standard_yaml(caseinfo)
+    #     AssertUtil().assertCommon(rsp)
+    #
+    # # 标签详情接口
+    # @pytest.mark.parametrize("caseinfo", read_testcase("GetLabelDetail.yaml"))
+    # def test_GetLabelDetail(self, caseinfo):
+    #     rsp = RequestUtil().standard_yaml(caseinfo)
+    #     AssertUtil().assertCommon(rsp)
